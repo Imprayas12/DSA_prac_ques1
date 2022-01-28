6299d29cf717535f71bd7726a0304a45873baf9c
Prayas Gautam <prayas12gautam@gmail.com>
2022-01-28 15:13:56 +0530

    Update Marks.java

--- src/Marks.java
+++ src/Marks.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
 
-public class Marks {
+class Marks{
+    public double subAvg(int [][]a,int x){
+        double sum=0;
+        for(int[] i:a){
+            sum+=i[x];
+        }
+        return sum/4;
+    }
+    public double stuAvg(int [][]a,int x){
+        double sum=0;
+        for(int i=0;i<5;i++){
+            sum+=a[x][i];
+        }
+        return sum/5;
+    }
+}
+public class Q3 {
     public static void main(String[] args) {
+        int[][] a=new int[4][5];
+        Marks obj = new Marks();
         Scanner sc = new Scanner(System.in);
-        int[][] MARKS = new int[4][5];
-        double st
-        for(int i=0;i<4;i++){
-            System.out.printf("Enter marks of Student %d :",i+1);
-            for(int j=0;j<5;j++){
-                System.out.printf("In subject %d :",j+1);
-                MARKS[i][j]= sc.nextInt();
+        for(int i=0;i< a.length;i++){
+            System.out.print("Enter marks of "+(i+1)+" student: ");
+            for(int j=0;j<a[i].length;j++){
+                a[i][j]= sc.nextInt();
             }
         }
-        for (int i = 0;i <4; i++) {
-            for (int j = 0; j < 5; j++) {
-
-            }
+        sc.close();
+        for(int i=0;i<5;i++){
+            System.out.println("Average marks of subject "+(i+1)+" is "+obj.subAvg(a,i));
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.println("Average marks of student "+(i+1)+" is "+obj.stuAvg(a,i));
         }
     }
 }
